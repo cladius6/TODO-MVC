aaddbcb62eb1d683086ade77d33dcb0ce6581f60
cladius6 <d1f1.6@protonmail.com>
2023-02-09 08:24:41 +0100

    Add basic swagger

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -4,11 +4,16 @@ import {
   getTodos,
   postTodos,
 } from "../controllers/app.controller.ts";
+import swaggerUi from "npm:swagger-ui-express";
+import swaggerDocument from "../swagger/swagger.json" assert { type: "json" };
 
 const router = Router();
 
 router.get("/health-check", getHealthCheck);
 router.get("/todos", getTodos);
 router.post("/todos", postTodos);
+
+router.use("/swagger", swaggerUi.serve);
+router.get("/swagger", swaggerUi.setup(swaggerDocument));
 
 export default router;
